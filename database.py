913a4953c0e445870bf18c2ea8446d7ba4d8afd0
Alena <30665926+Alenale@users.noreply.github.com>
2018-05-10 00:49:32 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import ZODB, ZODB.FileStorage
+from BTrees import OOBTree
 import transaction
 
 
@@ -8,6 +9,8 @@ class DataStorage():
         self.db = ZODB.DB(self.storage)
         self.connection = self.db.open()
         self.db_root = self.connection.root()
+        if ref_name not in self.db_root.keys():
+            self.db_root[ref_name] = OOBTree()
         self.ref_name = ref_name
         self.ref = self.db_root[ref_name]
 
@@ -16,12 +19,17 @@ class DataStorage():
 
     def __setitem__(self, value):
         self.ref = value
+        self._p_changed = True
 
     def delete_item(self, ref, key, i):
         del(self.db_root[ref][key][i])
 
     def delete_key(self, ref, key):
         del(self.db_root[ref][key])
+
+    def delete_ref(self, ref):
+        del(self.db_root[ref])
+        ref = None
 
     def finish(self):
         transaction.commit()
--- images.py
+++ images.py
@@ -1,8 +1,6 @@
 import requests
-import json
 import datetime
 import dpath
-import transaction
 from collections import defaultdict
 from database import DataStorage
 
@@ -18,9 +16,9 @@ class Images(DataStorage):
 
     # получить данные по координатам
     # вытащить из них id'ники локаций
+    # latitude = '41.845300'
+    # longitude = '12.524698'
     def get_location_id(self, lat, lon):
-        #latitude = '41.845300'
-        #longitude = '12.524698'
         url = 'https://api.instagram.com/v1/'
         access_token = "6918512031.3449bf9.51192f57c96c446fbb2ce0780a773d85"
         distance = '5000'
@@ -39,21 +37,15 @@ class Images(DataStorage):
     # https://www.instagram.com/explore/locations/{location_id}/?__a=1
     # path /graphql/location/edge_location_to_media/edges/0/node/edge_media_to_caption/edges/0/node/text
     # обращение к тексту картинки images_info['graphql']['location']['edge_location_to_media']['edges'][i]['node']['edge_media_to_caption']['edges'][0]['node']
-
     def get_images(self, location_id):
         text_list = []
-        #img_dic.fromkeys(['loc_name', 'images', 'time_inst'])
         image_info_url = 'https://www.instagram.com/explore/locations/'
         request_url = (image_info_url + location_id + '/?__a=1')
         print('Get request url: %s ' % (request_url))
         images_info = requests.get(request_url).json()
-        #length_info = sum(len(v) for v in images_info.values())
-        #length_items = sum(len(v) for v in images_info.items())
         ind_publication = 0
-        print("Length")
         length_info = len([element['node'] for element in images_info['graphql']['location']['edge_location_to_media']['edges']])
         tags = ['rome', 'thunder', 'lightning', 'storm','thunderbolt', 'thunderstorm', 'thunderstruck', 'thunderbirds', 'thunderclouds', 'lightning', 'thunderup', 'heatlightning']
-        # print(length_info)
         while ind_publication < length_info:
             ind_publication = str(ind_publication)
             is_video = dpath.util.get(images_info, '/graphql/location/edge_location_to_media/edges/' + ind_publication + '/node/is_video')
@@ -63,42 +55,37 @@ class Images(DataStorage):
             path_img = '/graphql/location/edge_location_to_media/edges/' + ind_publication + '/node/display_url'
             path_name = '/graphql/location/name'
             path_time = '/graphql/location/edge_location_to_media/edges/' + ind_publication + '/node/taken_at_timestamp'
-            # добавить условие, в котором будет слов в полученной строке без добавления в лист, вытягивать ссылку на изображение по i
             if (len(is_text) != 0) and (not is_video):
-                #print("i: " + i)
                 text = dpath.util.get(images_info, path_text)
                 img = dpath.util.get(images_info, path_img)
-                # фильтровать фотографии по наличию хэштегов
+                # поиск вхождения ключевых слов в описание
                 find_in_str = [x for x in tags if text.find(x) > -1]
-                # сохранить названия геолокации, ссылки, времени фотографии в бд в виде списка значений
+                # сохранить названия геолокации, ссылки, времени фотографии в бд в виде списка значений ключей
                 if len(find_in_str) > 0 :
                     # получить название геолокации и дату
+                    print("Have found!")
                     loc_name = dpath.util.get(images_info, path_name)
                     img_time = dpath.util.get(images_info, path_time)
                     time_inst = datetime.datetime.fromtimestamp(img_time).strftime('%Y-%m-%d %H:%M:%S')
                     img_dict['loc_name'].append(loc_name)
                     img_dict['image'].append(img)
                     img_dict['time_inst'].append(time_inst)
-
-
             else:
                 print("No text or it's video")
             ind_publication = int(ind_publication)
             ind_publication += 1
-
         DataStorage.__setitem__(self, img_dict)
-        # print(DataStorage.__getitem__(self, 'image'))
         DataStorage.finish(self)
 
-
-print("Input latitude and logitude")
-lat = input()
-lon = input()
-img = Images(lat, lon)
-loc_id = []
-loc_id = img.get_location_id(lat, lon)
-for ind in loc_id:
-    img.get_images(ind)
+if __name__ == '__main__':
+    print("Input latitude and logitude")
+    lat = input()
+    lon = input()
+    img = Images(lat, lon)
+    loc_id = []
+    loc_id = img.get_location_id(lat, lon)
+    for ind in loc_id:
+        img.get_images(ind)
 
 
 
